3cf2a506b3cf30b2ee7f2fdd225cef9fd996bb7f
Savelenko <savelenko@gmail.com>
2022-12-14 21:32:52 +0100

    Skeletal solution

---
+++ FSharp.Control.Fold/Fold/Fold.fs
@@ -0,0 +1,7 @@
+﻿namespace FSharp.Control.Fold
+
+type FoldCont<'a,'b,'r> =
+    abstract Apply : ('state -> 'a -> 'state) -> 'state -> ('state -> 'b) -> 'r
+
+type Fold<'a,'b> =
+    abstract Elim : FoldCont<'a,'b,'r> -> 'r
