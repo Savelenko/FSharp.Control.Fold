4e79d0a0fa704f476e0cd9d667fe061f62b3c998
Savelenko <Savelenko@users.noreply.github.com>
2022-12-23 12:22:51 +0100

    A few benchmarks for sequences and composed folds

--- FSharp.Control.Fold/Benchmarks/Benchmarks.fs
+++ FSharp.Control.Fold/Benchmarks/Benchmarks.fs
@@ -1,4 +1,5 @@
-﻿open BenchmarkDotNet.Attributes
+﻿open System
+open BenchmarkDotNet.Attributes
 open BenchmarkDotNet.Running
 open BenchmarkDotNet.Configs
 open FSharp.Control.Fold
@@ -30,7 +31,33 @@ type SeqAndFoldSimple () =
     member _.FoldConstant () =
         Seq.foldl (Fold.ret "Constant result") integers
 
+    [<Benchmark (Description = "Fold return CE"); BenchmarkCategory ("Constant")>]
+    member _.FoldConstantCE () =
+        integers
+        |> Seq.foldl (fold {
+            return "Constant result"
+        })
+
+[<CategoriesColumn; GroupBenchmarksBy (BenchmarkLogicalGroupRule.ByCategory)>]
+type SeqAndFoldComposed () =
+
+    let integers = seq { 1 .. 10_000 }
+
+    [<Benchmark (Baseline = true, Description = "Seq.fold manual sum and max"); BenchmarkCategory ("SumMax")>]
+    member _.SeqFoldManualSumMax () =
+        Seq.fold (fun (s, m) a -> (s + a, max m a)) (0, Int32.MinValue) integers
+
+    [<Benchmark (Description = "Fold sum and max CE"); BenchmarkCategory ("SumMax")>]
+    member _.FoldSumMax () =
+        integers
+        |> Seq.foldl (fold {
+            let! sum = makeFold (fun s a -> s + a) 0 id
+            and! max = makeFold (fun m a -> max m a) Int32.MinValue id
+            return (sum, max)
+        })
+
 [<EntryPoint>]
 let main args =
     let _ = BenchmarkRunner.Run<SeqAndFoldSimple> ()
+    let _ = BenchmarkRunner.Run<SeqAndFoldComposed> ()
     0
