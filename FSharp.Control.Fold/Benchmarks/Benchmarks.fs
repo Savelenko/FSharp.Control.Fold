79e22bf3786bcbece8f71f3201627f5f0e28fa09
Savelenko <Savelenko@users.noreply.github.com>
2022-12-23 11:52:05 +0100

    Benchmarks for sequences and single folds

--- FSharp.Control.Fold/Benchmarks/Benchmarks.fs
+++ FSharp.Control.Fold/Benchmarks/Benchmarks.fs
@@ -1,2 +1,36 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Benchmarks will be here"
+﻿open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Configs
+open FSharp.Control.Fold
+
+open Fold
+
+[<CategoriesColumn; GroupBenchmarksBy (BenchmarkLogicalGroupRule.ByCategory)>]
+type SeqAndFoldSimple () =
+
+    let integers = seq { 1 .. 10_000 }
+
+    [<Benchmark (Description = "Seq.sum"); BenchmarkCategory ("Sum")>]
+    member _.SeqSum () =
+        Seq.sum integers
+
+    [<Benchmark (Baseline = true, Description = "Seq.fold manual sum"); BenchmarkCategory ("Sum")>]
+    member _.SeqFoldManualSum () =
+        Seq.fold (fun sum a -> sum + a) 0 integers
+
+    [<Benchmark (Description = "Fold manual sum"); BenchmarkCategory ("Sum")>]
+    member _.FoldManualSum () =
+        Seq.foldl (makeFold (fun sum a -> sum + a) 0 id) integers
+
+    [<Benchmark (Baseline = true, Description = "Seq.fold constant function"); BenchmarkCategory ("Constant")>]
+    member _.SeqFoldConstant () =
+        Seq.fold (fun c _ -> c) "Constant result" integers
+
+    [<Benchmark (Description = "Fold.ret"); BenchmarkCategory ("Constant")>]
+    member _.FoldConstant () =
+        Seq.foldl (Fold.ret "Constant result") integers
+
+[<EntryPoint>]
+let main args =
+    let _ = BenchmarkRunner.Run<SeqAndFoldSimple> ()
+    0
