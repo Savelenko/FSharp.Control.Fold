647007e27c961efd71674d044451bee73815c860
Savelenko <Savelenko@users.noreply.github.com>
2022-12-23 10:51:52 +0100

    Change the convention of example output formatting

--- FSharp.Control.Fold/Examples/Examples.fs
+++ FSharp.Control.Fold/Examples/Examples.fs
@@ -10,7 +10,7 @@ let main args =
 
     // Example 1
     let length = makeFold (fun count _ -> count + 1) 0 id
-    printfn $"The length of the list is {List.foldl length list}"
+    printfn $"Example 1: {List.foldl length list}"
 
     // Example 2
     let lengthAndSum = fold {
@@ -18,6 +18,6 @@ let main args =
         and! sum = makeFold (+) 0 id
         return (length, sum)
     }
-    printfn $"The length and sum of all elements of the list are {List.foldl lengthAndSum list}"
+    printfn $"Example 2: {List.foldl lengthAndSum list}"
 
     0
